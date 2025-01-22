8907b0711bb3995d64d8ac73efa0bbaec304fca5
arjunm24 <neekhraarjun24@gmail.com>
2025-01-22 21:42:10 +0530

    Create Problem Statement.cpp

---
+++ Leetcode/Medium/Subsets/Problem Statement.cpp
@@ -0,0 +1,9 @@
+// **LeetCode Medium Question: Subsets**
+//
+// **Description:**
+// Given an integer array `nums` of unique elements, return all possible subsets (the power set).
+// The solution set must not contain duplicate subsets. Return the subsets in any order.
+//
+// **Example:**
+// Input: nums = [1, 2, 3]
+// Output: [[], [1], [2], [1,2], [3], [1,3], [2,3], [1,2,3]]
