411164df79afd6dc8bacba57495def221e064622
arjunm24 <neekhraarjun24@gmail.com>
2025-01-22 21:55:56 +0530

    Create Problem Statement.cpp

---
+++ Leetcode/Medium/Grid Unique Paths/Problem Statement.cpp
@@ -0,0 +1,11 @@
+// **LeetCode Medium Problem: Grid Unique Paths**
+//
+// **Description:**
+// A robot is located at the top-left corner of a `m x n` grid (marked 'Start' in the diagram below).
+// The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+// How many possible unique paths are there?
+//
+// **Example:**
+// Input: m = 3, n = 7
+// Output: 28
+// Explanation: There are 28 unique paths to reach the bottom-right corner.
